f95cad49cc462a9b682f507dd26c1c8fb5f7a01a
William Colten Anderson <coltenanderson95@gmail.com>
2024-06-16 10:30:21 -0500

    Added update operation to ScheduleSlot

--- HCAPatientPortalPOC/Controllers/ScheduleSlotsController.cs
+++ HCAPatientPortalPOC/Controllers/ScheduleSlotsController.cs
@@ -72,4 +72,71 @@ public class ScheduleSlotsController : Controller
             }  
             return View();
         }
+
+        // GET: ScheduleSlot/Update/id
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var slot = await _context.ScheduleSlots.FindAsync(id);
+            if (slot == null)
+            {
+                return NotFound();
+            }
+            List<Provider> providers = await _context.Providers.ToListAsync();
+            ViewData["Providers"] = providers;
+            return View(slot);
+        }
+
+        // POST: ScheduleSlot/Update/id
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int id, Dictionary<string, string> dict)
+        {
+            ScheduleSlot slot;
+            try
+            {
+                slot = Validator.ValidateScheduleSlot(dict);
+            }
+            catch(Exception e)
+            {
+                // TODO: route to error page
+                Console.WriteLine(e.Message);
+                return View();
+            }
+            if (id != slot.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(slot);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ScheduleSlotExists((int)slot.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        private bool ScheduleSlotExists(int id)
+        {
+            return _context.ScheduleSlots.Any(e => e.Id == id);
+        }
 }
