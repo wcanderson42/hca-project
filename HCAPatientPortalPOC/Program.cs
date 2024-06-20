36cf6b11cfc91ae6ddafe2d24f1a2bac96855fcd
William Colten Anderson <coltenanderson95@gmail.com>
2024-06-19 20:17:10 -0500

    removed unused swashbuckle package

--- HCAPatientPortalPOC/Program.cs
+++ HCAPatientPortalPOC/Program.cs
@@ -1,5 +1,4 @@
 using HCAPatientPortalPOC.Models;
-using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,10 +15,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    
-
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
